1a1359b8c7d969fa999d3d6be9a3662e0de9d7bb
webdron <webdron@gmail.com>
2024-08-13 17:32:33 +0300

    9step Finished(15)

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,15 +4,15 @@ import random
 
 def check_generate():
     n = random.randint(1, 100)
-    div = 2
-    res = 1
+    divider = 2
+    result = 1
     if n <= 1:
         check = 'no'
     else:
-        while res != 0:
-            res = n % div
-            div = div + 1
-        if div == n + 1 and res == 0:
+        while result != 0:
+            res = n % divider
+            divider = divider + 1
+        if divider == n + 1 and result == 0:
             check = 'yes'
         else:
             check = 'no'
