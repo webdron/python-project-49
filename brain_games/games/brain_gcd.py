a89d59980c2a44faeb5b8da48bbe49021d739634
webdron <webdron@gmail.com>
2024-08-15 12:23:27 +0300

    9step Finished(22)

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -9,4 +9,4 @@ def get_question_and_right_answer():
     n2 = random.randint(1, 100)
     right_answer = math.gcd(n1, n2)
     question = f"Question: {n1} {n2}"
-    return right_answer, question
+    return str(right_answer), question
